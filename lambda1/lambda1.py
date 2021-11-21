0e6bc51be8270139778fff88411ea50d35c02c67
Robert Wan <37159376+rw417@users.noreply.github.com>
2021-11-20 22:19:41 -0500

    update lambda1 function

--- lambda1/lambda1.py
+++ lambda1/lambda1.py
@@ -3,6 +3,7 @@ import boto3
 import botocore
 import time
 import datetime
+from polygon import RESTClient
 
 # SETUP LOGGING
 import logging
@@ -19,6 +20,62 @@ LOG.addHandler(logHandler)
 
 dynamodb = boto3.resource('dynamodb')
 dynamoTable = dynamodb.Table('stockprice')
+
+# script to pull stock price from Polygon API
+def get_current_time_local_time_zone():
+    """[get_current_time_local_time_zone]
+
+    Returns:
+        [string]: ["2021-11-15"]
+    """
+    return time.strftime("%Y-%m-%d", time.localtime())
+
+
+def timestring_to_datetime(timestring):
+    """[transform into the datetime type]
+
+    Args:
+        ts ([int]): [time integer]
+
+    Returns:
+        [datetime]: [datetime object]
+    """
+    timestring /= 1000
+    t = datetime.datetime.fromtimestamp(timestring).strftime("%Y-%m-%d %H:%M")
+    return t
+
+
+def get_price():
+    """[extract ticker, open price, close price, high price, low price, and volumn]"""
+    key = "CG0mfIrTZlytZFDyMr1kOGcIpNtj4HpT"
+
+    with RESTClient(key) as client:
+        start_time = "2021-11-16"
+        # end_time = "2021-11-16"
+        end_time = get_current_time_local_time_zone()
+        response = client.stocks_equities_aggregates(
+            "AAPL", 1, "minute", start_time, end_time, unadjusted=False
+        )
+
+        ticker = response.ticker
+        
+        prices = pd.DataFrame.from_dict(response.results)
+        prices.rename(columns = {
+            't':'timestamp',
+            'o':'open',
+            'c':'close',
+            'h':'high',
+            'l':'low',
+            'v':'volume',
+            'vw':'volume_weighted_price',
+            'n':'number_of_trades'
+        },
+        inplace=True)
+        prices['time_str'] = prices.timestamp.apply(lambda x: timestring_to_datetime(x))
+        prices.drop(columns=['a','op'], inplace=True)
+
+        return prices
+
 
 def write_to_dynamo(df):
     """Put Pandas DF to dynamodb"""
