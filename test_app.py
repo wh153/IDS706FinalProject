4032baa1bed0449a91c9dfc6b9edd4365d73fec1
nansuwang <nansuwang@gmail.com>
2021-12-07 13:31:54 -0500

    CICD v3

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ from os import path
 def test_necessary_files():
     assert path.exists("./API_Polygon/API_polygon.py")
     assert path.exists("./lambda1/lambda_function.py")
-    assert path.exists("./lambda1/lambda1_local.py.py")
+    assert path.exists("./lambda1/lambda1_local.py")
     assert path.exists("./lambda1/load_history.py")
