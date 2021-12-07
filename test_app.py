8c15b9476d10a383f871848c40d8dca16eb36d83
Nansu Wang <wns@MacBook-Pro.local>
2021-12-07 12:57:09 -0500

    CICD v1

---
+++ test_app.py
@@ -0,0 +1,8 @@
+from os import path
+
+
+def test_necessary_files():
+    assert path.exists("API_Polygon\API_polygon.py")
+    assert path.exists("lambda1\lambda_function.py")
+    assert path.exists("lambda1\lambda1_local.py.py")
+    assert path.exists("lambda1\load_history.py")
